009c972472964e0679ccabb5442ab1cd3a1ce8d1
Lythr0nax <lythr0naxe@gmail.com>
2023-02-04 01:29:12 +0200

    Guys

---
+++ scenes/tech/scr_behind_area.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var affected_node : Node2D
+@export_group("Technical")
+@export var change_time := 0.25
+@export var final_alpha := 0.5
+
+
+func _on_body_entered(body: Node2D) -> void:
+	var tw := create_tween()
+	tw.tween_property(affected_node, "modulate:a", final_alpha, change_time)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	var tw := create_tween()
+	tw.tween_property(affected_node, "modulate:a", 1.0, change_time)
