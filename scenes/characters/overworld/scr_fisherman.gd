f2b88ec635b72d94f9ace2978257ad683b1dd8f6
Lythr0nax <lythr0naxe@gmail.com>
2023-03-15 22:03:31 +0200

    Fishermen

---
+++ scenes/characters/overworld/scr_fisherman.gd
@@ -0,0 +1,60 @@
+@tool
+extends Node2D
+
+@export_enum("green", "blue", "yellow", "red", "bikeghost") var type: int = 0: set = _set_type
+@export var line_length := 30: set = _set_line_lenght
+@export var flip := false: set = _set_flip
+@export var randomise_men := false: set = _randomise_men
+
+
+func _set_line_lenght(to: int) -> void:
+	line_length = to
+	var line : Line2D = get_node_or_null("Pole/Line2D")
+	if not is_instance_valid(line): return
+	
+	line.points[1].y = line_length
+
+
+func _set_type(to: int) -> void:
+	type = to
+	
+	var sprite : Sprite2D = get_node_or_null("Face")
+	if not is_instance_valid(sprite): return
+	match to:
+		0:
+			sprite.region_rect = Rect2i(0, 0, 16, 16)
+		1:
+			sprite.region_rect = Rect2i(16, 0, 16, 16)
+		2:
+			sprite.region_rect = Rect2i(0, 16, 16, 16)
+		3:
+			sprite.region_rect = Rect2i(16, 16, 16, 16)
+		4:
+			sprite.region_rect = Rect2i(48, 32, 16, 16)
+
+
+func _set_flip(to: bool) -> void:
+	flip = to
+	
+	var sprite : Sprite2D = get_node_or_null("Face")
+	if is_instance_valid(sprite):
+		sprite.flip_h = to
+	var pole := get_node_or_null("Pole")
+	if is_instance_valid(pole):
+		pole.scale.x = remap(float(not flip), 0, 1, -1, 1)
+		pole.position.x = remap(float(not flip), 0, 1, -2, 2)
+
+
+func _on_interact() -> void:
+	var dialpr : int = DAT.get_data("fisher_progress", 0)
+	dialpr = wrapi(dialpr + 1, 1, 6)
+	DAT.set_data("fisher_progress", dialpr)
+	SOL.dialogue("fisherman_%s" % dialpr)
+
+
+func _randomise_men(_to: bool) -> void:
+	if not is_inside_tree(): return
+	var men := get_tree().get_nodes_in_group("fishermen")
+	for man in men:
+		man.type = randi() % 4
+
