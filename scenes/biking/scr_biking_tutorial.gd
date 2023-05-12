0b11e853716f858ea6f022903fa58ab4246ba231
Lythr0nax <lythr0naxe@gmail.com>
2023-05-12 23:53:31 +0300

    Stupid bug

--- scenes/biking/scr_biking_tutorial.gd
+++ scenes/biking/scr_biking_tutorial.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+var transing := false
+
 
 func _input(event: InputEvent) -> void:
+	if transing: return
 	if event.is_action_pressed("ui_accept"):
+		transing = true
 		LTS.level_transition("res://scenes/biking/scn_biking_game.tscn")
