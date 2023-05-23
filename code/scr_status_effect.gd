c3be65be5d73aa472ad44fc8ffb5933f5e1dbf04
Lythr0nax <lythr0naxe@gmail.com>
2023-05-24 00:32:44 +0300

    Damn

--- code/scr_status_effect.gd
+++ code/scr_status_effect.gd
@@ -3,5 +3,5 @@ class_name StatusEffect extends Resource
 # quick thing to store status effect applying in BattlePayload
 
 @export var name := ""
-@export var strength := 1
-@export var duration := 1.0
+@export var strength := 1.0
+@export var duration := 1
