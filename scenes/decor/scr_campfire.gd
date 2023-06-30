684dabff6fb2053ee71b7bbb137d823b614345e3
Murumart <lythr0naxe@gmail.com>
2023-06-30 18:53:30 +0300

    Campfire can cook now

--- scenes/decor/scr_campfire.gd
+++ scenes/decor/scr_campfire.gd
@@ -1,14 +1,20 @@
 class_name Campfire extends Node2D
 
 @export var lit := false
+var exchanges := {}
 
 @onready var timer := $Timer as Timer
 var flames := preload("res://scenes/vfx/scn_vfx_battle_burning.tscn")
+var explode := preload("res://scenes/vfx/scn_vfx_explosion.tscn")
 
 
 func _ready() -> void:
 	timer.start(1.0)
 	timer.timeout.connect(process)
+	exchanges["cookmeat"] = Exchange.new().set_input(["meat"]
+	).set_output(["meat_cooked"])
+	exchanges["cookegg"] = Exchange.new().set_input(["egg"]
+	).set_output(["eggshell", "egg_cooked"])
 
 
 func _on_inspected() -> void:
@@ -19,8 +25,31 @@ func _on_inspected() -> void:
 			SOL.dialogue_closed.connect(
 				func(): if SOL.dialogue_choice == "yes": light(), CONNECT_ONE_SHOT)
 		return
-	
 	SOL.dialogue("insp_lit_campfire")
+	SOL.dialogue_closed.connect(func():
+		if SOL.dialogue_choice == "yes":
+			SOL.dialogue_box.adjust(
+			"campfire_exchanges", 0, "choices", Math.reaap(exchanges.keys(), "exit"))
+			SOL.dialogue("campfire_exchanges")
+			SOL.dialogue_closed.connect(func():
+				if SOL.dialogue_choice in exchanges.keys():
+					var key := SOL.dialogue_choice
+					SOL.dialogue_box.dial_concat("campfire_exchange_confirm", 0, [key])
+					SOL.dialogue("campfire_exchange_confirm")
+					SOL.dialogue_closed.connect(func():
+						if SOL.dialogue_choice == "yes":
+							var success := exchanges[key].exchange(
+							DAT.get_character("greg").inventory) as bool
+							if not success:
+								exchanges[key].state(Exchange.Statements.CRITERIA)
+							else:
+								add_child(explode.instantiate())
+						elif SOL.dialogue_choice == "info":
+							exchanges[key].state(Exchange.Statements.CRITERIA)
+							exchanges[key].state(Exchange.Statements.RETURNS)
+					, CONNECT_ONE_SHOT)
+			, CONNECT_ONE_SHOT)
+		, CONNECT_ONE_SHOT)
 
 
 func light() -> void:
