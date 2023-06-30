febb0607abd02686a4c45725504fd6129ca8a55c
Murumart <lythr0naxe@gmail.com>
2023-06-30 18:52:44 +0300

    Exchange system (crafting, trading...)

---
+++ code/scr_exchange.gd
@@ -0,0 +1,77 @@
+class_name Exchange extends Resource
+# every rpg needs a crafting system!!!!!!!!
+
+enum Statements {CRITERIA, RETURNS}
+
+@export var silver_required := 0
+@export var input : PackedStringArray = []
+@export var output : PackedStringArray = []
+
+
+func exchange(inventory: Array) -> bool:
+	print("exchanging!")
+	var has_input := true
+	for i in input:
+		if not i in inventory:
+			has_input = false
+	if DAT.get_data("silver", 0) < silver_required:
+		has_input = false
+	if not has_input:
+		SOL.dialogue("exchange_missing_input")
+		return false
+	SOL.dialogue("exchange_success")
+	for i in input:
+		inventory.erase(i)
+	DAT.incri("silver", -silver_required)
+	for i in output:
+		inventory.append(i)
+		SOL.dialogue_box.dial_concat("exchange_item", 0, [DAT.get_item(i).name])
+		SOL.dialogue("exchange_item")
+	return true
+
+
+func state(what: Statements) -> void:
+	var informations := []
+	# criterion strings
+	if what == Statements.CRITERIA:
+		if silver_required:
+			informations.append("- %s silver" % silver_required)
+		for i in input:
+			informations.append("- %s" % DAT.get_item(i).name)
+	elif what == Statements.RETURNS:
+		for i in output:
+			informations.append("- %s" % DAT.get_item(i).name)
+	
+	# TITLE: this exchange requires: LB%s LB%s
+	# SISU: %s LB%s LB%s
+	var divisions := [[]]
+	for i in informations.size():
+		var criterion := informations[i] as String
+		if i < 2: # divisions[0] is the title, only 2 %s
+			divisions[0].append(criterion)
+		else:
+			if (i + 1) % 3 == 0: divisions.append([])
+			divisions[floori((i + 1) / 3.0)].append(criterion)
+	var title := "exchange_criteria_title" if what == Statements.CRITERIA else "exchange_returns_title"
+	for i in divisions.size():
+		if i == 0:
+			while divisions[i].size() < 2:
+				divisions[i].append("") # need empty string to make %s disappear
+			SOL.dialogue_box.dial_concat(title, 0, divisions[i])
+			SOL.dialogue(title)
+		else:
+			while divisions[i].size() < 3:
+				divisions[i].append("")
+			SOL.dialogue_box.dial_concat("exchange_criteria_sisu", 0, divisions[i])
+			SOL.dialogue("exchange_criteria_sisu")
+
+
+func set_input(to: PackedStringArray) -> Exchange:
+	input = to
+	return self
+
+
+func set_output(to: PackedStringArray) -> Exchange:
+	output = to
+	return self
+
