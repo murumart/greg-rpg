8ac9cabce6dcca1a318f59e1da01ccebc1cda96c
Murumart <lythr0naxe@gmail.com>
2023-11-28 21:34:26 +0200

    Mafia house

--- scenes/rooms/scr_town_park.gd
+++ scenes/rooms/scr_town_park.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	tarikas.inspected.connect(_on_tarikas_inspected)
 	tarikas_lines()
 	guru.inspected.connect(_on_guru_inspected)
-	if DAT.get_data("battles", 0) < 1:
+	if DAT.get_data("battles", 0) < 25:
 		guru.queue_free()
 
 
