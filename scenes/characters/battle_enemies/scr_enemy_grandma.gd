ea6aa914189e0b0eeda4af170e8937df33cdfe9e
Murumart <lythr0naxe@gmail.com>
2023-12-01 20:09:07 +0200

    Grandma fix and also somerhing

--- scenes/characters/battle_enemies/scr_enemy_grandma.gd
+++ scenes/characters/battle_enemies/scr_enemy_grandma.gd
@@ -7,12 +7,15 @@ func _ready() -> void:
 
 func act() -> void:
 	var greg : BattleActor = reference_to_opposing_array[0]
-	if greg.character.health_perc() > 0.5:
-		ai_action()
-	else:
-		SND.play_song("")
-		DAT.set_data("fought_grandma", true)
-		DAT.incri("intro_dialogue_progress", 1)
-		LTS.gate_id = LTS.GATE_EXIT_BATTLE
-		await get_tree().create_timer(1.0).timeout
-		LTS.level_transition(LTS.ROOM_SCENE_PATH % "grandma_after_fight_staredown")
+	message.emit("grandma attacked greg!")
+	for i in 400:
+		greg.hurt(1, Genders.VAST)
+		await get_tree().process_frame
+		if greg.character.health_perc() < 0.1:
+			break
+	SND.play_song("")
+	DAT.set_data("fought_grandma", true)
+	DAT.incri("intro_dialogue_progress", 1)
+	LTS.gate_id = LTS.GATE_EXIT_BATTLE
+	await get_tree().create_timer(1.0).timeout
+	LTS.level_transition(LTS.ROOM_SCENE_PATH % "grandma_after_fight_staredown")
