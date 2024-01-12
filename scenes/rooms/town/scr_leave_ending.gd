afa369b258f1b77a3c7c68a16b04e25ff0c5c43e
Murumart <lythr0naxe@gmail.com>
2024-01-12 23:20:41 +0200

    Roads that look like level exits now tell you why they aren't really

---
+++ scenes/rooms/town/scr_leave_ending.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var approach_area := $ApproachArea as Area2D
+
+
+func _ready() -> void:
+	if DAT.get_data("leave_ending_offered", false):
+		queue_free()
+		return
+	approach_area.body_entered.connect(func(_a):
+		approach_area.queue_free()
+		SOL.dialogue("leave_ending_check")
+		DAT.set_data("leave_ending_offered", true)
+		SOL.dialogue_closed.connect(func():
+			if SOL.dialogue_choice == &"yes":
+				# TODO put something normal here
+				$"../../Greg".queue_free()
+		, CONNECT_ONE_SHOT) 
+	)
