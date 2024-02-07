bc541144707b631aba01fb2e36de1e9b53d0625e
Murumart <lythr0naxe@gmail.com>
2024-02-07 18:35:18 +0200

    Lakeside fish warning

--- scenes/rooms/scr_room_lakeside.gd
+++ scenes/rooms/scr_room_lakeside.gd
@@ -1,5 +1,7 @@
 extends Room
 
+@onready var canvas_modulate: CanvasModulate = $CanvasModulate
+
 
 func _ready() -> void:
 	super._ready()
@@ -11,3 +13,12 @@ func _on_pole_interacted() -> void:
 		SND.play_song("")
 		LTS.level_transition("res://scenes/fishing/scn_fishing_minigame.tscn")
 		, CONNECT_ONE_SHOT)
+
+
+func _on_enemy_encounter_area_body_entered(body: Node2D) -> void:
+	create_tween().tween_property(canvas_modulate, "color", Color(
+			0.60029411315918, 0.9275940656662, 0.99999982118607), 0.4)
+
+
+func _on_enemy_encounter_area_body_exited(body: Node2D) -> void:
+	create_tween().tween_property(canvas_modulate, "color", Color.WHITE, 0.4)
