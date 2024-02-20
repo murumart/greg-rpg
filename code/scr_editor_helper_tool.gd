67889ff06ddb0cf04c3e4feb2b4cb378dec51ea7
Murumart <lythr0naxe@gmail.com>
2024-02-20 22:12:51 +0200

    editor helper fix

--- code/scr_editor_helper_tool.gd
+++ code/scr_editor_helper_tool.gd
@@ -7,20 +7,21 @@ class_name EditorHelperTool extends Node
 
 
 func _ready() -> void:
-	if not Engine.is_editor_hint() and not OS.has_feature("editor"):
+	if not Engine.is_editor_hint() or not OS.has_feature("editor"):
 		queue_free()
 		return
 	auto()
 
 
 func auto() -> void:
-	if not Engine.is_editor_hint() and not OS.has_feature("editor"):
+	if not Engine.is_editor_hint() or not OS.has_feature("editor"):
 		return
 	set_project_version()
 
 
 func set_project_version() -> void:
-	print("setting version")
 	var DAT_SCRIPT := load("res://autoload/scr_data.gd")
-	ProjectSettings.set_setting("application/config/version", DAT_SCRIPT.version_str())
+	if not ProjectSettings.get_setting("application/config/version") == DAT_SCRIPT.version_str():
+		ProjectSettings.set_setting("application/config/version", DAT_SCRIPT.version_str())
+		print("setting version")
 	ProjectSettings.save()
