4aadd84f3c9d6a453442472214fca90557a4af61
Murumart <lythr0naxe@gmail.com>
2024-06-13 21:13:39 +0300

    licensing and credits update

--- scenes/gui/scr_main_menu.gd
+++ scenes/gui/scr_main_menu.gd
@@ -29,8 +29,7 @@ func _ready() -> void:
 		$VBoxContainer/MailButton/MailPanel/RichTextLabel.text = HateMail.letter()
 		$VBoxContainer/MailButton.visible = true
 	button_focuses()
-	$VBoxContainer/CreditsButton/TextPanel/RichTextLabel.text = FileAccess.open("res://credits.txt"
-			, FileAccess.READ).get_as_text()
+	_load_credits()
 	if randf() <= 0.008:
 		for x in buttons:
 			var mov := ScreenEdgeBounceComponent.new()
@@ -146,3 +145,39 @@ func _on_credits_button_pressed() -> void:
 	$VBoxContainer/CreditsButton/TextPanel.show()
 	$VBoxContainer/CreditsButton/TextPanel/RichTextLabel/AutoscrollComponent.reset.call_deferred()
 	$VBoxContainer/CreditsButton/TextPanel/RichTextLabel/AutoscrollComponent.check.call_deferred()
+
+
+func _load_credits() -> void:
+	var text := FileAccess.open("res://credits.txt", FileAccess.READ).get_as_text()
+	text += "\n\n"
+	text += FileAccess.open("res://LICENSE.txt", FileAccess.READ).get_as_text()
+	text += "\n\n"
+	text += Engine.get_license_text()
+	text += "\n"
+	text += "Portions of this software are copyright © <year> The FreeType Project (www.freetype.org). All rights reserved.\n"
+	text += """
+
+	Copyright (c) 2002-2020 Lee Salzman
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+\n"""
+	text += """
+
+	Copyright The Mbed TLS Contributors
+
+	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.\n
+"""
+	text += var_to_str(Engine.get_license_info())
+	text += "\n"
+	text += var_to_str(Engine.get_copyright_info())
+
+	$VBoxContainer/CreditsButton/TextPanel/RichTextLabel.text = text
+
