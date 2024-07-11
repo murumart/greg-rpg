2d1f28d1b2b9058f6f7de4211a732ac4fffae7a8
Murumart <lythr0naxe@gmail.com>
2024-07-12 00:04:32 +0300

    char levels up slower needs testing

--- code/scr_character.gd
+++ code/scr_character.gd
@@ -97,7 +97,7 @@ func health_perc() -> float:
 
 # xp to get to the specified level
 func xp2lvl(lvl: int) -> int:
-	return roundi(Math.LEVEL_UP_CURVE.sample(remap(lvl, 1, 99, 0.0, 1.0)) * lvl)
+	return roundi(Math.LEVEL_UP_CURVE.sample_baked(remap(lvl, 1, 99, 0.0, 1.0)) * lvl)
 
 
 # currently only greg can functionally gain xp and level up
