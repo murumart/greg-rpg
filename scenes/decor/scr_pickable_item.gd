2d443cc1f425daf32df045beec40064a1d7f6ac9
Murumart <lythr0naxe@gmail.com>
2024-07-24 21:46:36 +0300

    bronze rifle findable

--- scenes/decor/scr_pickable_item.gd
+++ scenes/decor/scr_pickable_item.gd
@@ -1,13 +1,23 @@
 class_name PickableItem extends InteractionArea
 
 @export var item_type := &""
+@export var save := false
 
 
 func _ready() -> void:
 	assert(ResMan.items.has(item_type), "item type doesn't exist")
+	if save and DAT.get_data(get_save_key(), false):
+		queue_free()
+		return
 	$Sprite2D.texture = ResMan.get_item(item_type).texture
 
 
 func _on_on_interact() -> void:
 	DAT.grant_item(item_type)
+	if save:
+		DAT.set_data(get_save_key(), true)
 	queue_free()
+
+
+func get_save_key() -> String:
+	return "pickable_item_" + name + "_in_" + LTS.get_current_scene().name + "_picked"
