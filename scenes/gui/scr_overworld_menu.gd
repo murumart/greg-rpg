74061669686257f9ecf2e0619447b611e5c98a15
webcatz <june.webcatz@gmail.com>
2024-07-24 20:51:28 +0400

    better/wosrse equip signifiers

--- scenes/gui/scr_overworld_menu.gd
+++ scenes/gui/scr_overworld_menu.gd
@@ -245,6 +245,24 @@ func load_items() -> void:
 			{"item": true, "custom_pass_function": item_names})
 	var silver_text := "p. silver:\n" if party_size() > 1 else "silver: "
 	silver_counter.text = str(silver_text, DAT.get_data("silver", 0))
+	
+	for button: Button in item_container.get_children():
+		var item: Item = ResMan.get_item(button.reference)
+		
+		if item.use == Item.Uses.WEAPON:
+			if party(current_tab).weapon and party(current_tab).weapon != button.reference:
+				var current: int = ResMan.get_item(party(current_tab).weapon).payload.attack_increase
+				if item.payload.attack_increase > current:
+					button.add_theme_color_override("font_color", Color.GREEN)
+				elif item.payload.attack_increase < current:
+					button.add_theme_color_override("font_color", Color.RED)
+		elif item.use == Item.Uses.ARMOUR:
+			if party(current_tab).armour and party(current_tab).armour != button.reference:
+				var current: int = ResMan.get_item(party(current_tab).armour).payload.defense_increase
+				if item.payload.defense_increase > current:
+					button.add_theme_color_override("font_color", Color.GREEN)
+				elif item.payload.defense_increase < current:
+					button.add_theme_color_override("font_color", Color.RED)
 
 
 func item_names(opt := {}) -> void:
