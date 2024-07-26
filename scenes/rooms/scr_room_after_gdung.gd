bee2d00705381d8cf80216a326bbe0c2bfbaec53
Murumart <lythr0naxe@gmail.com>
2024-07-26 18:36:32 +0300

    bye bike

--- scenes/rooms/scr_room_after_gdung.gd
+++ scenes/rooms/scr_room_after_gdung.gd
@@ -1,13 +1,10 @@
 extends Room
 
 @onready var ending_1 := $Ending1
-@onready var bike: Node2D = $InteriorTiles/Decor/Bike
 
 
 func _ready() -> void:
 	super()
-	if not 0 in DAT.get_data("bike_ghosts_fought", []):
-		bike.queue_free()
 	if LTS.gate_id == LTS.GATE_EXIT_BATTLE:
 		select_ending()
 		return
