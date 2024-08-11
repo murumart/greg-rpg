5e80d409bacbdb8a3180ae4a9a5c8b043aace0b5
Murumart <lythr0naxe@gmail.com>
2024-08-11 21:57:32 +0300

    save screen openable while menu open

--- scenes/gui/scr_overworld_menu.gd
+++ scenes/gui/scr_overworld_menu.gd
@@ -52,9 +52,12 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	# open the save menu (no idea why this is controlled here)
 	if Input.is_action_just_pressed("quick_save") or Input.is_action_just_pressed("quick_load"):
-		if DAT.player_capturers.is_empty():
+		if (DAT.player_capturers.is_empty()
+				or (DAT.player_capturers.size() == 1
+					and "overworld_menu" in DAT.player_capturers)):
 			if not saving_disabled:
 				SOL.save_menu(Input.is_action_just_pressed("quick_load"))
+				close_requested.emit()
 	if not visible:
 		return
 	get_viewport().set_input_as_handled()
