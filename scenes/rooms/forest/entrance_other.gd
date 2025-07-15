a7dea8b85ec7cfd0e6d0e8d06f7e9e1f4b5e4a39
murumart <lythr0naxe@gmail.com>
2025-07-15 19:26:02 +0300

    menace

--- scenes/rooms/forest/entrance_other.gd
+++ scenes/rooms/forest/entrance_other.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var woodsman: OverworldCharacter = $Woodsman
 @onready var mobulate: ColorContainer = $"../CanvasModulateGroup/Mobulate"
+@onready var forest_music: AudioStreamPlayer2D = $"../Areas/RoomGate2/AudioStreamPlayer2D"
+@onready var greg: PlayerOverworld = $"../Greg"
 
 
 func _ready() -> void:
@@ -26,7 +28,29 @@ func _ready() -> void:
 
 func _rose_cutscene() -> void:
 	DAT.capture_player("cutscene")
-	DAT.set_data(&"got_flower_rose", true)
 	var tw := create_tween()
 	SND.play_song("")
 	tw.tween_property(mobulate, ^"color", Color(0.66, 0.31, 0.862), 2.0)
+	tw.tween_callback(func() -> void: greg.animate("walk_down", true))
+	tw.parallel().tween_property(forest_music, ^"volume_db", -80, 1.0)
+	tw.parallel().tween_property(greg, ^"global_position", woodsman.global_position + Vector2.DOWN * 12, 1.0)
+	tw.tween_callback(func() -> void:
+		greg.animate("walk_up")
+		SND.play_song("extremophile", 0.5, {pitch_scale = 0.89})
+	)
+	tw.tween_interval(0.2)
+	await tw.finished
+	SOL.dialogue("woods_guy_has_materials")
+	await SOL.dialogue_closed
+	DAT.set_data(&"got_flower_rose", true)
+	var inv := ResMan.get_character(&"greg").inventory
+	for i in 20:
+		inv.erase(&"rose_petals")
+		inv.erase(&"rose_thorns")
+	inv.append(&"flower_rose")
+	SND.play_song("")
+	await Math.timer(0.5)
+	SOL.dialogue("woods_guy_after_2")
+	await SOL.dialogue_closed
+	await Math.timer(1.0)
+	SOL.dialogue("woods_guy_after_3")
