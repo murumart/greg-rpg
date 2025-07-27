1d07647c0d30ef29b63ef0ff1d1350a12195c366
murumart <lythr0naxe@gmail.com>
2025-07-27 17:39:39 +0300

    yeas

--- autoload/scr_screen_overlay.gd
+++ autoload/scr_screen_overlay.gd
@@ -260,9 +260,10 @@ func set_dialogue_mode(mode: int) -> void:
 	match mode:
 		0:
 			dialogue_box.textbox.remove_theme_font_override("normal_font")
+			dialogue_box.textbox.remove_theme_font_size_override("normal_font_size")
 		1:
 			dialogue_box.textbox.add_theme_font_override("normal_font", preload("res://fonts/gregorious_smaller.tres"))
-			dialogue_box.textbox.add_theme_font_size_override("normal_font", 16)
+			dialogue_box.textbox.add_theme_font_size_override("normal_font_size", 16)
 
 
 var _ldisplay_label_y := 0.0
--- scenes/rooms/sg/sg_room.gd
+++ scenes/rooms/sg/sg_room.gd
@@ -1,8 +1,11 @@
 extends Node
 
+@export var collision_hide: CanvasItem
+
 
 func _ready() -> void:
 	SOL.set_dialogue_mode(1)
+	collision_hide.hide()
 
 
 func _exit_tree() -> void:
