e4268070d51490368ef28d02fb3a851ad4b857b3
murumart <lythr0naxe@gmail.com>
2025-08-11 09:22:53 +0300

    sgy6

--- code/path_circle_rotator.gd
+++ code/path_circle_rotator.gd
@@ -1,11 +1,13 @@
 extends Node
 
 @export var path: Path2D
-@export var followers: Array[PathFollow2D]
 @export var speed := 0.1
+
+var followers: Array[PathFollow2D]
 
 
 func _ready() -> void:
+	followers.assign(path.get_children())
 	var r := 1.0 / followers.size()
 	for f in followers.size():
 		followers[f].progress_ratio = r * f
