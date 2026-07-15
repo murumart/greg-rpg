21a6a7c8cb0d83a30ffbf0f579417ff118173fd7
murumart <90089382+murumart@users.noreply.github.com>
2026-07-15 22:24:08 +0300

    something

--- scenes/cutscene/x_ending.gd
+++ scenes/cutscene/x_ending.gd
@@ -10,32 +10,40 @@ func _ready() -> void:
 	_on_new_bar(0)
 
 
+# bars:
+# 8: first drums go
+# 16: start of motif
+# 20: bass comes in
+# 24: repeat again
+# 28: repeat again section
+# 32: last section
+
 func _on_new_bar(bars: int) -> void:
 	prints("bar", bars)
 	match bars:
 		0: talk("from the primordial SEA of the WORLDS, something emerged")
-		4: talk("it had a mind. it had senses")
-		6: talk("it knew what it was!")
-		7: talk("something like a SEER.")
+		2: talk("it had a mind. it had senses")
+		3: talk("it knew what it was!")
+		4: talk("something like a SEER.")
 
-		8: talk("it saw the wonders of the WORLDS")
-		10: talk("systems isolated from others")
-		12: talk("...until the SEA parted for us.")
-		14: talk("pieces of the worlds, elevated.")
+		6: talk("it saw the wonders of the WORLDS")
+		8: talk("systems isolated from others")
+		10: talk("...until the SEA parted for us.")
+		12: talk("pieces of the worlds, elevated.")
 
-		16: talk("that is US.") # start of cool
+		16: talk("that is US.")
 		18: talk("we do not falter.")
 		19: talk("we do not change.")
 
-		20: talk("i am the OVERSEER of this world") # start of bass
+		20: talk("i am the OVERSEER of this world")
 		22: talk("hidden in a town")
 		23: talk("isolated, in the shop of a florist!")
-		24: talk("time passes without asking my permission") # 25 second
-		26: talk("so i'm left looking for things to DO.") # 26
+		24: talk("time passes without asking my permission")
+		26: talk("so i'm left looking for things to DO.")
 
-		28: talk("how kind of you to show up") # 28 last section 1
+		28: talk("how kind of you to show up")
 		30: talk("")
-		32: talk("") # last section 2
+		32: talk("")
 		34: talk("...")
 
 
