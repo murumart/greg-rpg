f06b3122dff88d12bdef6782c268df82488a13a7
mart <90089382+murumart@users.noreply.github.com>
2026-09-14 13:45:36 +0300

    house float now credits

--- scenes/rooms/grandma_house/house_float.gd
+++ scenes/rooms/grandma_house/house_float.gd
@@ -2,6 +2,7 @@ extends Node
 
 const M = preload("res://scenes/vfx/x_menacing.gd")
 const SP = preload("res://scenes/gui/x_speech_buble.gd")
+const Credits = preload("res://scenes/gui/scr_end_credits.gd")
 
 const GIGLE = preload("res://sounds/x/gigle.tres")
 
@@ -13,9 +14,11 @@ const GIGLE = preload("res://sounds/x/gigle.tres")
 @onready var second_music: AudioStreamPlayer = $"../SecondMusic"
 @onready var ambient: AudioStreamPlayer = $"../Ambient"
 @onready var overlay: AnimationPlayer = $Overlay
+@onready var end_credits: Credits = $EndCredits
 
 
 func _ready() -> void:
+	remove_child(end_credits)
 	fade.show()
 	DAT.capture_player("cutscene")
 	_c1.call_deferred()
@@ -24,9 +27,13 @@ func _ready() -> void:
 func _c1() -> void:
 	menacing.particles(0.75)
 	var tw := create_tween().set_trans(Tween.TRANS_CUBIC)
+	greg.animate("walk_down", true, 0.25)
 	tw.tween_property(fade, ^"modulate:a", 0.0, 6.0).set_ease(Tween.EASE_IN)
+	tw.parallel().tween_property(greg, "position:y", greg.position.y, 5.0).from(greg.position.y - 12)
+	tw.parallel().tween_callback(greg.animate.bind("walk_down")).set_delay(5.0)
 	tw.tween_interval(1.0)
 	speech_buble.spam_sound = menacing.speech_snd
+	tw.tween_callback(greg.animate.bind("walk_left")).set_delay(1.25)
 	tw.tween_property(menacing, "global_position:y", greg.global_position.y - 20, 1.0).set_ease(Tween.EASE_OUT)
 	tw.tween_callback(func() -> void:
 		var dlg := DialogueBuilder.new()
@@ -58,4 +65,13 @@ func _c2() -> void:
 	second_music.play()
 	var tw := create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
 	tw.tween_property(menacing, "global_position:y", menacing.global_position.y - 120, 1.0)
+	tw.tween_callback(greg.animate.bind("walk_down")).set_delay(3.0)
 	overlay.play(&"end")
+	tw.tween_callback(func() -> void:
+		var e: Array = DIR.gej(3, [])
+		e.append(4)
+		DIR.sej(3, e)
+		SOL.add_ui_child(end_credits)
+		await get_tree().process_frame
+		end_credits.play()
+	).set_delay(18)
